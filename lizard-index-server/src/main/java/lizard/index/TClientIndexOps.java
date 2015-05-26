eb302e64d717fcc8f66546456690428285db88cd
Andy Seaborne <andy@seaborne.org>
2015-05-26 15:33:03 +0100

    Interface to TClientIndex

---
+++ lizard-index-server/src/main/java/lizard/index/TClientIndexOps.java
@@ -0,0 +1,67 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  See the NOTICE file distributed with this work for additional
+ *  information regarding copyright ownership.
+ */
+
+package lizard.index;
+
+import java.util.Iterator ;
+
+import lizard.comms.ConnState ;
+import lizard.comms.Connection ;
+import lizard.system.ComponentTxn ;
+import lizard.system.Pingable ;
+import org.apache.jena.atlas.lib.Tuple ;
+import org.seaborne.tdb2.store.NodeId ;
+
+interface TClientIndexOps extends Connection, ComponentTxn, Pingable 
+{
+    @Override
+    public void start() ;
+    
+    @Override
+    public void stop() ;
+    
+    /** Insert a tuple - return true if it was really added, false if it was a duplicate */
+    public boolean add(Tuple<NodeId> tuple) ;
+
+    /** Delete a tuple - return true if it was deleted, false if it didn't exist */
+    public boolean delete(Tuple<NodeId> tuple) ;
+    
+    @Override
+    public void ping() ;
+
+    public Iterator<Tuple<NodeId>> find(Tuple<NodeId> pattern) ;
+
+    static Tuple<NodeId> tupleAny4 = Tuple.createTuple(NodeId.NodeIdAny, NodeId.NodeIdAny, NodeId.NodeIdAny, NodeId.NodeIdAny) ; 
+    static Tuple<NodeId> tupleAny3 = Tuple.createTuple(NodeId.NodeIdAny, NodeId.NodeIdAny, NodeId.NodeIdAny) ; 
+    
+    /** return an iterator of everything */
+    default public Iterator<Tuple<NodeId>> all() { return find(tupleAny3) ; }  
+    
+    @Override
+    public ConnState getConnectionStatus() ;
+
+    @Override
+    public String label() ;
+
+    @Override
+    public void setConnectionStatus(ConnState status) ;
+    
+    @Override
+    public void close() ;
+}
+
+    
