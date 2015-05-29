15b52f8ee4f96b43e12323b447dcf72b50b68092
Andy Seaborne <andy@seaborne.org>
2015-05-29 13:45:48 +0100

    Move from lizard.conf.build to lizard.build

---
+++ lizard-cluster/src/main/java/lizard/build/LzBuildZk.java
@@ -0,0 +1,69 @@
+/**
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  See the NOTICE file distributed with this work for additional
+ *  information regarding copyright ownership.
+ */
+
+package lizard.build;
+
+import lizard.conf.* ;
+
+import org.apache.curator.test.TestingServer ;
+import org.apache.jena.atlas.logging.FmtLog ;
+import org.apache.zookeeper.server.ServerConfig ;
+import org.apache.zookeeper.server.ZooKeeperServerMain ;
+import org.seaborne.dboe.migrate.L ;
+import org.slf4j.Logger ;
+
+public class LzBuildZk {
+    private static Logger logConf = Config.logConf ;
+    
+    static TestingServer zkTestServer;
+    
+    public static String zookeeper(ConfCluster confCluster, NetHost here) {
+        if ( confCluster.zkServer.size() == 0 )
+            throw new LzConfigurationException("No zookeeper details in cluster configuration") ;
+        if ( confCluster.zkServer.size() > 1 )
+            throw new LzConfigurationException("Multiple zookeeper details in cluster configuration") ;
+        ConfZookeeper confZookeeper = confCluster.zkServer.get(0) ;
+        zookeeper(confZookeeper);
+        return "localhost:"+confZookeeper.port  ;
+    }
+
+    public static void zookeeper(ConfZookeeper confZookeeper) {
+        if ( confZookeeper.isEphemeral() ) {
+            FmtLog.info(logConf, "Zookeeper (ephemeral): %d", confZookeeper.port) ;
+            zookeeperSimple(confZookeeper.port) ;
+            return ;
+        }
+
+        FmtLog.info(logConf, "Zookeeper %s : %d", confZookeeper.zkConfDir, confZookeeper.port) ;
+        ServerConfig config = new ServerConfig();
+        config.parse(new String[] {Integer.toString(confZookeeper.port), confZookeeper.zkConfDir}) ;
+        ZooKeeperServerMain zk = new ZooKeeperServerMain();
+        L.async(()-> {
+            try { zk.runFromConfig(config) ; }
+            catch (Exception e) { FmtLog.warn(logConf, "Failed to run zookeeper: "+e.getMessage(), e); }
+        }) ;
+    }
+    
+    public static void zookeeperSimple(int port) {
+        try { zkTestServer = new TestingServer(port) ; }
+        catch (Exception e) { e.printStackTrace(); }
+    }
+
+
+
+}
+
---
+++ lizard-cluster/src/main/java/lizard/build/LzDeploy.java
@@ -0,0 +1,67 @@
+/**
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  See the NOTICE file distributed with this work for additional
+ *  information regarding copyright ownership.
+ */
+
+package lizard.build;
+
+import lizard.cluster.Cluster ;
+import lizard.cluster.Platform ;
+import lizard.conf.ConfCluster ;
+import lizard.conf.NetHost ;
+import lizard.query.LzDataset ;
+
+import org.apache.jena.query.Dataset ;
+import org.seaborne.dboe.base.file.Location ;
+import org.seaborne.tdb2.setup.StoreParams ;
+
+public class LzDeploy {
+    
+    /** Deploy the server-side of a configuration here */
+    public static void deployServers(ConfCluster confCluster, NetHost here) {
+        String zkConnect = LzBuildZk.zookeeper(confCluster, here) ;
+        Cluster.createSystem(zkConnect);
+        Location baseLocation = null ;
+        
+        if ( confCluster.fileroot == null )
+            baseLocation = Location.mem() ;
+        else
+            baseLocation = Location.create(confCluster.fileroot) ; 
+
+        Platform platform = new Platform() ;
+        StoreParams params = StoreParams.getDftStoreParams() ; 
+        
+        // Each server has it's own journal - it'll be part of the distributed transaction.
+        LzBuilderNodeServer.build(platform, baseLocation, params, confCluster, here);
+        LzBuilderIndexServer.build(platform, baseLocation, params, confCluster, here);
+        
+        platform.start(); 
+
+    }
+    
+    public static Dataset deployDataset(ConfCluster confCluster, NetHost here) {
+        Location baseLocation = null ;
+        if ( confCluster.fileroot == null )
+            baseLocation = Location.mem() ;
+        else
+            baseLocation = Location.create(confCluster.fileroot) ; 
+        Location locationQueryServer = baseLocation.getSubLocation("query") ;
+        LzDataset lzdsg = LzBuilderDataset.build(confCluster, locationQueryServer) ;
+        if ( lzdsg == null )
+            return null ;
+        return LzBuilderDataset.dataset(lzdsg) ;
+    }
+}
+
